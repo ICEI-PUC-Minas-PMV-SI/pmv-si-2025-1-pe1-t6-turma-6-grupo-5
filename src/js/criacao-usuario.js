13017de978c08994f3cba91e058dcb72210956a1
Tiago Dias <tiago0214@gmail.com>
2025-06-02 22:26:43 -0300

    Add a manager passcode

--- src/js/criacao-usuario.js
+++ src/js/criacao-usuario.js
@@ -3,10 +3,27 @@ const name = document.getElementById("name")
 const cpf = document.getElementById("cpf")
 const password = document.getElementById("password")
 const rePassword =  document.getElementById("re-password")
+const user = document.getElementById("usuario")
+const showPasscode = document.getElementById("showPasscode")
+const passcode = document.getElementById("passcode")
 
 const formCad =  document.getElementById("formCad")
 
 let users = JSON.parse(localStorage.getItem('users')) || []
+
+let manager = false
+
+user.addEventListener('change', (v) =>{
+    const actualValue = v.target.value
+
+    if(actualValue === 'Gestor'){
+        showPasscode.style.display = 'block'
+        manager = true
+    }else{
+        showPasscode.style.display = 'none'
+        manager = false
+    }
+})
 
 formCad.addEventListener("submit",(event) =>{
     event.preventDefault()
@@ -16,12 +33,25 @@ formCad.addEventListener("submit",(event) =>{
         name: name.value,
         cpf: cpf.value,
         password: password.value,
-        rePassword: rePassword.value
+        rePassword: rePassword.value,
+        passcode: manager ? passcode.value : ''
     })
 })
 
 function createNewUser(data){
-    const { email,name, cpf, password, rePassword } = data
+    const { email,name, cpf, password, rePassword, passcode } = data
+
+    if(manager && passcode != 90){
+        alert('Código de gestor inválido')
+
+        return false
+    }
+
+    const emailAlreadyExist = verifyDuplicateEmail(email)
+
+    if(emailAlreadyExist){
+        alert('Já existe um usuário com este email')
+    }
 
     if(email === '' || email === undefined || email === null)
         return false
@@ -36,26 +66,37 @@ function createNewUser(data){
     if(password.toString() != rePassword.toString())
         return false
 
+    
+
     users.push({
         email,
         name,
         cpf,
         password,
+        manager,
         session: false
     })
+
+    resetFields()
 
     return localStorage.setItem('users', JSON.stringify(users))
 }
 
-function verifyDuplicateEmail(){
-
-    // need to verify if there is a user with the same email
-    console.log(users)
+function resetFields(){
+    email.innerHTML=''
+    name.innerHTML=''
+    cpf.innerHTML=''
+    password.innerHTML=''
+    rePassword.innerHTML=''
+    passcode.innerHTML=''
 }
 
-// function getUsers(){
-//     users = localStorage.getItem('users') || []
-// }
+function verifyDuplicateEmail(email){
+    const emailAlreadyExist = users.some(user => {
+        return user.email === email
+    })
 
-// getUsers()
-verifyDuplicateEmail()
+    return emailAlreadyExist
+}
+
+console.log(users)
